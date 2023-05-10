754007c4374a3bd4d050a7171b259d2bfa875f24
Saurabh Madheshiya <Saurabh.Madheshiya@fenergo.com>
2023-05-10 10:57:15 +0200

    Latest

--- TDDKata/TDDKata/UnitTest1.cs
+++ TDDKata/TDDKata/UnitTest1.cs
@@ -7,11 +7,39 @@ namespace TDDKata
     public class UnitTest1
     {
         [Fact]
-        public void AddDigitsAndShouldBeCorrectResult()
+        public void AddNoDigitsAndShouldBeCorrectResult()
         {
             //Arrange
             string digits = "";
             int expected = 0;
+            var result = new Calc();
+
+            //Act
+            var actual = result.Add(digits);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void AddDigitsAndShouldBeCorrectResult()
+        {
+            //Arrange
+            string digits = "1,2,3";
+            int expected = 6;
+            var result = new Calc();
+
+            //Act
+            var actual = result.Add(digits);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+        [Fact]
+        public void AddDigitsWithMultipleCommasAndShouldBeCorrectResult()
+        {
+            //Arrange
+            string digits = "1,,2,,3";
+            int expected = 6;
             var result = new Calc();
 
             //Act
