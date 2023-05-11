3e1ca86852360e37b55bc4d9b59d5f65a8ffe33e
Saurabh Madheshiya <Saurabh.Madheshiya@fenergo.com>
2023-05-11 10:25:37 +0200

    Added different delimiter case

--- TDDKata/TDDKataCalc/Calc.cs
+++ TDDKata/TDDKataCalc/Calc.cs
@@ -1,4 +1,6 @@
-﻿namespace TDDKataCalc
+﻿using System.Collections.Generic;
+
+namespace TDDKataCalc
 {
     public class Calc
     {
@@ -8,12 +10,16 @@
             if (digits == string.Empty) return 0;
 
             if (digits.IndexOf('\n') > -1) { digits = digits.Replace('\n', ','); }
+            if (digits.IndexOf(';') > -1) { digits = digits.Replace(';', ','); }
 
             string[] arrDigits = digits.Split(',');
             if (arrDigits.Length == 0) return -1;
             int totalSum = 0;
             foreach (var item in arrDigits)
             {
+                if (string.IsNullOrWhiteSpace(item)) { continue; }
+                if (ListOfDelimiters().Contains(item)) { continue; }
+
                 int result;
                 if (int.TryParse(item, out result))
                     totalSum += result;
@@ -23,6 +29,10 @@
                 }
             }
             return totalSum;
+        }
+        private List<string> ListOfDelimiters()
+        {
+            return new List<string> { ";", "//" };
         }
     }
 }
